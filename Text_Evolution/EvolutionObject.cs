301c95e09bf0bb1d3a9d1a1720b029a141eb213e
Daniil-Demyanenko <daniil-demyanenko1@mail.ru>
2020-05-04 16:00:35 +0300

    Добавьте файлы проекта.

---
+++ Text_Evolution/EvolutionObject.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Evolution
+{
+    class EvolutionObject
+    {
+        /// <summary>
+        /// Фраза.
+        /// </summary>
+        public string content;
+
+        /// <summary>
+        /// Инициализация объекта.
+        /// </summary>
+        /// <param name="content">Фраза.</param>
+        public EvolutionObject(string content)
+        {
+            this.content = content;
+        }
+        /// <summary>
+        /// Инициальзация пустым значением
+        /// </summary>
+        public EvolutionObject()
+        {
+            this.content = "";
+        }
+
+        /// <summary>
+        /// Возвращает кофициент подобия поля content параметру.
+        /// </summary>
+        /// <param name="s">строка для сравнения.</param>
+        /// <returns></returns>
+        public int SimilarityCoefficient(string s)
+        {
+            int k = 0;
+            if (content.Length <= s.Length)
+            {
+                for (int i = 0; i < content.Length; i++)
+                    if (content[i] == s[i]) k++;
+            }
+            else
+            {
+                for (int i = 0; i < s.Length; i++)
+                    if (content[i] == s[i]) k++;
+            }
+            return k;
+        }
+    }
+}
---
+++ Text_Evolution/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Evolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!?:;\"'().,"; //Алфавит для перебора
+            string end = Console.ReadLine(); //Фраза, которая должна получиться
+            EvolutionObject[] population = new EvolutionObject[10]; //"Популяция" фраз
+            int N = 0; //Номер поколения
+            Random rand = new Random();
+
+
+        }
+    }
+}
